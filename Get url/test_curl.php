a67959a839010f8d9234472844d5badf11020c71
vis <vis@praktikant1>
2018-02-22 12:05:44 +0200

    Get url 2.0

--- Get url/test_curl.php
+++ Get url/test_curl.php
@@ -5,27 +5,26 @@ function get_data($url) {
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0(X11; Linux x86_64;rv:10.0) Gecko/20100101 Firefox/10.0');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_HTTPHEADER, array('custom-header-name: custom-header-value','another-custom-header: another value')); //setting custom header
+	//curl_setopt($curl, CURLOPT_HTTPHEADER, array('custom-header-name: custom-header-value','another-custom-header: another value')); //setting custom header
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	$data = curl_exec($ch);
 	curl_close($ch);
 	return $data;
 }
 $var= get_data("https://www.google.bg/search?q=miro&start=10");
-global $matches,$pieces;
+global $matches,$pieces,$matches2;
 $links =array();
-preg_match_all('~<a(.*?)href="http([^"]+)"(.*?)>~', $var, $matches);
+preg_match_all('~<cite(.*?)class="_Rm">(.*?)</cite>~', $var, $matches);
 
-$length= count($matches[2]);
-	for($i=0;$i<$length;$i++)
-	{
-		 $pieces = explode("http", $matches[2][$i]);
-		for ($j=0; $j < count($pieces); $j++) {
+$length= count($matches[1]);
+    for ($i=0; $i < $length; $i++) { 
+	echo $matches[2][$i],'<br>';
+    }
 
-		 		if(strpos($pieces[$j],"www.google")===false) $links[]=$pieces[$j];
-		}
-	}
-for ($i=0; $i <count($links) ; $i++) {
-	echo $links[$i],'<br>';
-}
+    echo $length;
+
+
+
+
+
 ?>

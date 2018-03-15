9ce2541d1f61db57dc3edd68e2ed4b27a130b989
rado <radoslav192@gmail.com>
2018-03-15 13:49:20 +0200

    Bug with additional wrong url's fixed

--- Page check/is_page_bad.php
+++ Page check/is_page_bad.php
@@ -2,97 +2,101 @@
 
 function get_domain_name($url)
 {
-    $ret = "";
-    $cnt_lines = 0;
-    $chars = str_split($url);
+	$ret = "";
+	$cnt_lines = 0;
+	$chars = str_split($url);
 
-    foreach($chars as $let)
-    {
-        $ret .= $let;
-        if($let == '/') $cnt_lines++;
-        if($cnt_lines == 3) break;
-    }
+	foreach($chars as $let)
+	{
+		$ret .= $let;
+		if($let == '/') $cnt_lines++;
+		if($cnt_lines == 3) break;
+	}
 
-    return $ret;
+	return $ret;
 }
 
 function get_links_from_page($url)
 {
-    $text = file_get_contents($url, false);
-    $ret = array();
+	$text = file_get_contents($url, false);
+	$ret = array();
 
-    $length_of_string = strlen($text);
-    for($i = 0; $i < $length_of_string; $i++)
-    {
-        if($text[$i] != 'h') continue;
-        if($text[$i + 1] != 't') continue;
-        if($text[$i + 2] != 't') continue;
-        if($text[$i + 3] != 'p') continue;
-        if($text[$i + 4] != 's') continue;
-        if($text[$i + 5] != ':') continue;
+	$length_of_string = strlen($text);
+	
+	for($i = 0; $i < $length_of_string; $i++)
+	{
+		if($text[$i] != 'h') continue;
+		if($text[$i + 1] != 't') continue;
+		if($text[$i + 2] != 't') continue;
+		if($text[$i + 3] != 'p') continue;
+		if($text[$i + 4] != 's') continue;
+		if($text[$i + 5] != ':') continue;
 
-        $current_url = "";
-        $is_ok = true;
-        for($j = $i; $j < $length_of_string; $j++)
-        {
-            if($text[$j] == "\"") break;
-            if($text[$j] == ")") { $is_ok = false; break; }
-            $current_url .= $text[$j];
-        }
+		$current_url = "";
+		$is_ok = true;
+		for($j = $i; $j < $length_of_string; $j++)
+		{
+			if($text[$j] == "\"") break;
+			if($text[$j] == "'") break;
+			if($text[$j] == ")") { $is_ok = false; break; }
+			$current_url .= $text[$j];
+		}
 
-        if(!$is_ok) continue;
-        array_push($ret, $current_url);
-    }
+		if(!$is_ok) continue;
+		array_push($ret, $current_url);
+	}
 
-    for($i = 0; $i < $length_of_string; $i++)
-    {
-        if($text[$i] != 'h') continue;
-        if($text[$i + 1] != 't') continue;
-        if($text[$i + 2] != 't') continue;
-        if($text[$i + 3] != 'p') continue;
-        if($text[$i + 4] != ':') continue;
+	for($i = 0; $i < $length_of_string; $i++)
+	{
+		if($text[$i] != 'h') continue;
+		if($text[$i + 1] != 't') continue;
+		if($text[$i + 2] != 't') continue;
+		if($text[$i + 3] != 'p') continue;
+		if($text[$i + 4] != ':') continue;
 
-        $current_url = "";
-        $is_ok = true;
-        for($j = $i; $j < $length_of_string; $j++)
-        {
-            if($text[$j] == "\"") break;
-            if($text[$j] == ")") { $is_ok = false; break; }
-            $current_url .= $text[$j];
-        }
+		$current_url = "";
+		$is_ok = true;
+		for($j = $i; $j < $length_of_string; $j++)
+		{
+			if($text[$j] == "\"") break;
+			if($text[$j] == "'") break;
+			if($text[$j] == ")") { $is_ok = false; break; }
+			$current_url .= $text[$j];
+		}
 
-        if(!$is_ok) continue;
-        array_push($ret, $current_url);
-    }
+		if(!$is_ok) continue;
+		array_push($ret, $current_url);
+	}
 
-    $current_domain_name = get_domain_name($url);
-    if($current_domain_name[strlen($current_domain_name) - 1] != '/') $current_domain_name .= '/';
+	$current_domain_name = get_domain_name($url);
+	if($current_domain_name[strlen($current_domain_name) - 1] != '/') $current_domain_name .= '/';
 
-    for($i = 0; $i < $length_of_string; $i++)
-    {
-        if($text[$i] != 'h') continue;
-        if($text[$i + 1] != 'r') continue;
-        if($text[$i + 2] != 'e') continue;
-        if($text[$i + 3] != 'f') continue;
-        if($text[$i + 4] != '=') continue;
+	for($i = 0; $i < $length_of_string; $i++)
+	{
+		if($text[$i] != 'h') continue;
+		if($text[$i + 1] != 'r') continue;
+		if($text[$i + 2] != 'e') continue;
+		if($text[$i + 3] != 'f') continue;
+		if($text[$i + 4] != '=') continue;
 
-        $current_url = "";
-        $is_ok = true;
-        for($j = $i + 6; $j < $length_of_string; $j++)
-        {
-            if($text[$j] == "\"") break;
-            if($text[$j] == ")") { $is_ok = false; break; }
-            $current_url .= $text[$j];
-        }
+		$current_url = "";
+		$is_ok = true;
+		for($j = $i + 6; $j < $length_of_string; $j++)
+		{
+			if($text[$j] == "\"") break;
+			if($text[$j] == "'") break;
+			if($text[$j] == ")") { $is_ok = false; break; }
+			$current_url .= $text[$j];
+		}
 
-        $current_url = $current_domain_name . $current_url;
+		$current_url = $current_domain_name . $current_url;
 
-        if(!$is_ok) continue;
-        array_push($ret, $current_url);
-    }
+		if(!$is_ok) continue;
+		array_push($ret, $current_url);
+	}
 
-    $ret = array_unique($ret);
-    return $ret;
+	$ret = array_unique($ret);
+	return $ret;
 }
 
 $initial_url = "";
@@ -104,17 +108,18 @@ function dfs($url)
 {
 	global $initial_url, $list_of_urls, $steps, $used; 
 
-	if($steps >= 100) return;
 	if(array_key_exists($url, $used)) return;
-	
-	$steps++;
+
 	$used[$url] = true;
-		
+
 	if(get_domain_name($url) != $initial_url)
 	{
 		$list_of_urls[get_domain_name($url)] = true;
 		return;
 	}
+
+	if($steps >= 20) return;
+	$steps++;
 
 	$adj = get_links_from_page($url);
 	foreach($adj as $v)
@@ -124,7 +129,7 @@ function dfs($url)
 function is_page_bad($url)
 {
 	global $initial_url, $list_of_urls, $steps, $used; 
-	
+
 	$initial_url = get_domain_name($url);
 	$list_of_urls = array();
 	$steps = 0;
@@ -134,12 +139,19 @@ function is_page_bad($url)
 
 	/*foreach($list_of_urls as $lnk => $val)
 		echo $lnk . "\n";
-	*/
+	 */
 
 	if(count($list_of_urls) >= 10) return true;
 	else return false;
 }
 
-is_page_bad("https://stackoverflow.com/questions/141108/how-to-find-the-foreach-index");
+//$result = is_page_bad("https://www.quora.com/Which-website-has-the-most-ads");
+//echo (($result == 0) ? "It's good" : "-"); 
 
- ?>
+$Li = get_links_from_page("https://www.google.de/search?q=miro+bulgaria+plovdiv&start=0");
+//foreach($Li as &$s) $s = get_domain_name($s);
+$Li = array_unique($Li);
+
+foreach($Li as $s) 
+	print($s . "\n");
+?>
